ecdef72e63969610de61d80a79c29b0eefac4fe9
vikash-vinsol <vikashsoni@Vikashs-MacBook-Air.local>
2014-07-11 14:11:20 +0530

    Add a message in file

--- GIt_Two/ViewController.m
+++ GIt_Two/ViewController.m
@@ -17,6 +17,8 @@
             
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSLog(@"App runs successfully");
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
