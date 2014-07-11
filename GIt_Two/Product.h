1d36587e4da034d83cde1425c6c219f0463207f9
vikash-vinsol <vikashsoni@Vikashs-MacBook-Air.local>
2014-07-11 14:14:19 +0530

    Add product files

---
+++ GIt_Two/Product.h
@@ -0,0 +1,13 @@
+//
+//  Product.h
+//  GIt_Two
+//
+//  Created by Vikash Soni on 11/07/14.
+//  Copyright (c) 2014 Vikash Soni. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Product : NSObject
+
+@end
---
+++ GIt_Two/Product.m
@@ -0,0 +1,13 @@
+//
+//  Product.m
+//  GIt_Two
+//
+//  Created by Vikash Soni on 11/07/14.
+//  Copyright (c) 2014 Vikash Soni. All rights reserved.
+//
+
+#import "Product.h"
+
+@implementation Product
+
+@end
